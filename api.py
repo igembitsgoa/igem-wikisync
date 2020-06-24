3088712b5a2df748e29b6df4f622781efe22326b
Pranav Ballaney <ballaneypranav@gmail.com>
2020-06-24 23:40:51 +0530

    Hide password

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 import ssl
 import mechanicalsoup
+import os
 
 class MyAdapter(HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
@@ -10,10 +11,8 @@ class MyAdapter(HTTPAdapter):
                                        block=block,
                                        ssl_version=ssl.PROTOCOL_TLSv1)
 
-data = {
-    'username': 'ballaneypranav',
-    'password': 'Pza29kgNwLuyyz'
-}
+username = os.environ['IGEM_USERNAME']
+password = os.environ['IGEM_PASSWORD']
 
 browser = mechanicalsoup.StatefulBrowser()
 
@@ -23,8 +22,8 @@ browser.select_form('form[method="post"]')
 
 browser.get_current_form().print_summary()
 
-browser["username"] = data["username"]
-browser["password"] = data["password"]
+browser["username"] = username
+browser["password"] = password
 
 response = browser.submit_selected()
 
