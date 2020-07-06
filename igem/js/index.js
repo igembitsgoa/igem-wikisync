e0f7d2da71eed04a3aadcee12811b2facaf2b214
Pranav Ballaney <ballaneypranav@gmail.com>
2020-07-06 07:35:47 +0530

    Delete built folder

--- igem/js/index.js
+++
@@ -1,7 +0,0 @@
-var ids=['HQ_page','content','bodyContent','mw-content-text']
-for(var i=0;i<ids.length;i++)
-document.querySelector('#'+ids[i]).removeAttribute('id');var classes=['mw-content-ltr']
-for(var i=0;i<classes.length;i++)
-{var elements=document.querySelectorAll('.'+classes[i]);for(var j=0;j<elements.length;j++)
-{elements[j].classList.remove(classes[i]);}}
-alert('JS works!');
