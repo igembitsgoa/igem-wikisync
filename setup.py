05188191385a0005ab1e4ff3ed9c4085420ac16e
Pranav Ballaney <ballaneypranav@gmail.com>
2020-07-17 22:56:29 +0530

    Finally fixed SSL error

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='igem-wikisync',
-    version='0.0.0-alpha.7',
+    version='0.0.0-alpha.8',
     license='MIT',
     description='Automatically deploy iGEM Wikis.',
     long_description='%s\n%s' % (
@@ -73,10 +73,10 @@ setup(
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
+        'pyopenssl',
         'mechanicalsoup',
         'jsmin',
-        'pyyaml',
-        'pyopenssl'
+        'pyyaml'
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
