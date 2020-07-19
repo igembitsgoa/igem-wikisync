da34cd780a134f351415812b1d56a502b0642746
Pranav Ballaney <ballaneypranav@gmail.com>
2020-07-19 21:20:20 +0530

    Add tests

--- tests/test_igem_wikisync.py
+++
@@ -1,6 +0,0 @@
-
-from igem_wikisync.cli import main
-
-
-def test_main():
-    assert main([]) == 0
---
+++ tests/test_wikisync.py
@@ -0,0 +1,25 @@
+from igem_wikisync import wikisync
+import filecmp
+
+import mechanicalsoup
+
+def test_wikisync():
+
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open('https://igem.org')
+
+    team = 'BITSPilani-Goa_India'
+    src_dir = 'test_src'
+    build_dir = 'test_build'
+    assets = ['assets']
+
+    wikisync(
+        team=team,
+        src_dir=src_dir,
+        build_dir=build_dir,
+        assets=assets)
+
+    with open('test_build/index.html', 'r') as outfile:
+        with open('test_ref/index.html', 'r') as reference:
+
+            assert filecmp.cmp(outfile, reference)
