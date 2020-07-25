06bdf3f0ef229d2b086f0e662aa498275a8b4a44
Pranav Ballaney <ballaneypranav@gmail.com>
2020-07-25 15:56:31 +0530

    Version update to 0.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='igem-wikisync',
-    version='0.0.0-alpha.8',
+    version='0.0.0',
     license='MIT',
     description='Automatically deploy iGEM Wikis.',
     long_description='%s\n%s' % (
