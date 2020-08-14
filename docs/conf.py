4e13bb81d87d18e817f480594bb177a1e7866566
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-14 06:17:21 +0530

    Bump version: 0.0.11 → 0.0.12

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'igem-wikisync'
 year = '2020'
 author = 'Pranav Ballaney'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.11'
+version = release = '0.0.12'
 
 pygments_style = 'trac'
 templates_path = ['.']
