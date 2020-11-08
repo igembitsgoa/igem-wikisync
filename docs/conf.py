8420d6a135225219bd2ab0d7c931b6c794fb9183
Pranav Ballaney <ballaneypranav@gmail.com>
2020-11-08 13:41:25 +0530

    Bump version: 1.1.0-alpha2 → 1.1.0-alpha3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'igem-wikisync'
 year = '2020'
 author = 'Pranav Ballaney'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.1.0-alpha2'
+version = release = '1.1.0-alpha3'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/igem_wikisync/__init__.py
+++ src/igem_wikisync/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.0-alpha2'
+__version__ = '1.1.0-alpha3'
 from .wikisync import run
 
 __all__ = ['run']
